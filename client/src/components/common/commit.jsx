ab66db78594b0f725442e0efd94061f934076199
prajwallshetty <prajwal0shetty11@gmail.com>
2025-04-18 16:18:49 +0530

    done and dusted

---
+++ client/src/components/common/commit.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const commit = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default commit
--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: "",
-  api_key: "",
-  api_secret: "",
+  cloud_name: "dhmlah9l1",
+  api_key: "675261266523141",
+  api_secret: "6KvcmjDSKW5sXZj1r7cSzNcmXsU",
 });
 
 const storage = new multer.memoryStorage();

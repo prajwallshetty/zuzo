02d0a580ee30dbd83b9ab60dab1e5f16d7dc3282
prajwallshetty <prajwal0shetty11@gmail.com>
2025-04-18 16:20:38 +0530

    added img src

--- client/src/components/common/commit.jsx
+++ client/src/components/common/commit.jsx
@@ -6,6 +6,7 @@ const commit = () => {
       <h1>commit</h1>
       <h1>commit</h1>
       <video src="https://www.w3schools.com/html/mov_bbb.mp4"></video>
+      <img src="https://www.w3schools.com/html/img_girl.jpg" alt="aa" />
     </div>
   )
 }
